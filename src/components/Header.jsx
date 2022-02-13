d4bb5024e9e379dd3181e0c751d1e03c9b553e53
bonox2 <illiagoncharov99@gmail.com>
2022-02-13 18:08:02 -0500

    header fix

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,13 +3,15 @@ import { useAuth } from '../hooks/useAuth';
 function Header() {
   const { user, signout } = useAuth();
   return (
-    <header className="header">
+    <>
       {user && (
-        <button className="actionbtn" type="submit" onClick={() => signout()}>
-          Log out
-        </button>
+        <header className="header">
+          <button className="actionbtn" type="submit" onClick={() => signout()}>
+            Log out
+          </button>
+        </header>
       )}
-    </header>
+    </>
   );
 }
 
