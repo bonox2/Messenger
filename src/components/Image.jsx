8f15f17071a6619cabed7d1e906612cbd10aa1b6
bonox2 <illiagoncharov99@gmail.com>
2022-02-19 21:18:53 -0500

    picture update

--- src/components/Image.jsx
+++ src/components/Image.jsx
@@ -1,21 +1,10 @@
 import React from 'react';
-import { Image, StyleSheet } from 'react-native';
-import { profilePic } from "../img/profile.png";
+import profilePic from "../img/profile.png";
 
 
-const styles = StyleSheet.create({
-  profile_img: {
-    paddingTop: 50,
-  }
-});
 
 const ImageProfile = () => {
-  return (
-      <Image
-        style={styles.profile_img}
-        source={profilePic}
-      />
-  );
+  <img src={profilePic} />
 }
 
 export default ImageProfile;
